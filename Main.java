a5682487964af665cf2705d8404dc381761a0f15
276113033 <2185447945@qq.com>
2021-04-14 16:50:24 +0800

    第一次推送

---
+++ Main.java
@@ -0,0 +1,10 @@
+package day12;
+
+public class Main {
+    public static void main(String[] args) {
+        student st=new student();
+    }
+//    public String getName(){
+//
+//    }
+}
---
+++ student.java
@@ -0,0 +1,18 @@
+package day12;
+
+public class student {
+    public student(){
+        System.out.println("aaaaa");
+    }
+    private Integer no;
+///
+    private String name;
+
+    private String sex;
+
+    private String iphone;
+
+    private Double height;
+
+    private String Ass;
+}
